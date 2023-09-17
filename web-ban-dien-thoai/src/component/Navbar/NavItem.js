732de691603c332a680904af1df0c41c8ee30a1c
spidicey <82193435+spidicey@users.noreply.github.com>
2023-09-17 11:35:15 +0700

    first commit

---
+++ web-ban-dien-thoai/src/component/Navbar/NavItem.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import { ReactComponent as BellIcon } from '../../icons/bell.svg';
+import { ReactComponent as MessengerIcon } from '../../icons/messenger.svg';
+import { ReactComponent as CaretIcon } from '../../icons/caret.svg';
+import { ReactComponent as PlusIcon } from '../../icons/plus.svg';
+import './NavItem.scss'
+export function NavItem(props) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
+
+            {open && props.children}
+        </li>
+    );
+}
