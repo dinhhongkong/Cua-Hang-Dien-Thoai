7ee8e641078dd5bf583aed4840eca2e78ac29e59
spidicey <82193435+spidicey@users.noreply.github.com>
2023-09-27 23:16:28 +0700

    cập nhật giá theo số lượng giỏ hàng

--- SpringBoot-CuaHangDienThoai/src/main/resources/static/js/cart.js
+++ SpringBoot-CuaHangDienThoai/src/main/resources/static/js/cart.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let addToCartButtons = document.querySelectorAll(".plus");
   let minusToCartButtons = document.querySelectorAll(".minus");
   let deleteCartButtons = document.querySelectorAll(".remove-item");
+  const totalPrices = document.getElementById("total_prices");
+  console.log(totalPrices);
   // console.log(deleteCartButtons);
   addToCartButtons.forEach((addToCartButton) => {
     addToCartButton.addEventListener("click", () => {
@@ -25,10 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
           let currentValue = parseInt(quantityInput.value);
           let newValue = currentValue + 1;
           quantityInput.value = newValue;
+          updataTotalPrice();
           console.log("thêm vào giỏ hàng thành công");
         }
       };
-      xhr.send("productId=" + productId+ "&cal=plus");
+      xhr.send("productId=" + productId + "&cal=plus");
     });
   });
   minusToCartButtons.forEach((minusToCartButton) => {
@@ -57,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let newValue = currentValue - 1;
             quantityInput.value = newValue;
           }
-
+          updataTotalPrice();
           console.log("Trừ khỏi giỏ hàng thành công");
         }
       };
@@ -84,6 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
           // Xử lý thành công, có thể cập nhật giao diện người dùng
           if (parentElement) {
+            updataTotalPrice();
             parentElement.remove();
           }
           console.log("Xoá giỏ hàng thành công");
@@ -92,29 +96,69 @@ document.addEventListener("DOMContentLoaded", function () {
       xhr.send("productId=" + productId);
     });
   });
-});
 
-// function increaseQuantity(productId) {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("PUT", "/cart/", true);
-//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//     let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
-//     let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
-//     xhr.setRequestHeader(csrfHeader, csrfToken);
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4) {
-//             if (xhr.status === 200) {
-//                 // Xử lý thành công, có thể cập nhật giao diện người dùng
-//                 let quantityInput = document.getElementById(productId);
-//                 let currentValue = parseInt(quantityInput.value);
-//                 let newValue = currentValue + 1;
-//                 quantityInput.value = newValue;
-//             } else {
-//                 // Xử lý lỗi (nếu có)
-//                 alert("Đã xảy ra lỗi khi tăng số lượng sản phẩm.");
-//             }
-//         }
-//     };
-//     let data = "productId=" + encodeURIComponent(productId);
-//     xhr.send(data);
-// }
+  const checkBoxes = document.querySelectorAll(".custom-control-input");
+  checkBoxes.forEach((checkBox) => {
+    // console.log(checkBox);
+    checkBox.addEventListener("click", () => {
+      let isTrueSet = checkBox.value === "true";
+      checkBox.value = (!isTrueSet).toString();
+      // console.log(checkBox.value);
+      updataTotalPrice();
+      // console.log(test);
+      // if (checkBox.value === "true") {
+      //   let price = parseInt(
+      //     checkBox
+      //       .closest(".block__product-item__outer")
+      //       .querySelector(".product__price--show").innerText,
+      //     10,
+      //   );
+      //   let quantity = parseInt(
+      //     document.getElementById(
+      //       "value" +
+      //         checkBox
+      //           .closest(".block__product-item__outer")
+      //           .querySelector(".plus")
+      //           .getAttribute("data-product-id"),
+      //     ).value,
+      //     10,
+      //   );
+      //   console.log(price);
+      //   console.log(quantity);
+      // }
+    });
+  });
+
+  let buyBtn = document.querySelector(".btn-action");
+  // console.log(buyBtn);
+  buyBtn.addEventListener("click", () => {});
+
+  const updataTotalPrice = () => {
+    let totalPrice = 0;
+    checkBoxes.forEach((checkBox) => {
+      if (checkBox.value === "true") {
+        let price = parseInt(
+          checkBox
+            .closest(".block__product-item__outer")
+            .querySelector(".product__price--show").innerText,
+          10,
+        );
+        let quantity = parseInt(
+          document.getElementById(
+            "value" +
+              checkBox
+                .closest(".block__product-item__outer")
+                .querySelector(".plus")
+                .getAttribute("data-product-id"),
+          ).value,
+          10,
+        );
+        // console.log(price);
+        // console.log(quantity);
+        totalPrice += quantity * price;
+        console.log(totalPrice);
+      }
+    });
+    totalPrices.innerText = totalPrice + "đ";
+  };
+});
