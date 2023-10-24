fcf5232a7f444a8897937710d45f7f4187aacd71
Rangdog <92283489+Rangdog@users.noreply.github.com>
2023-10-24 09:13:52 +0700

    cmit admin
    them mot so chung nang cho trang admin

--- SpringBoot-CuaHangDienThoai/src/main/java/com/cuahangdienthoai/controller/DeviceController.java
+++ SpringBoot-CuaHangDienThoai/src/main/java/com/cuahangdienthoai/controller/DeviceController.java
@@ -1,24 +1,45 @@
 package com.cuahangdienthoai.controller;
-
+import org.springframework.core.io.Resource;
 import com.cuahangdienthoai.entity.Device;
+import com.cuahangdienthoai.service.BrandService;
 import com.cuahangdienthoai.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class DeviceController {
     private DeviceService deviceService;
+    private BrandService brandService;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+    public DeviceController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
 
     @Autowired
     public void setDeviceService(DeviceService deviceService) {
         this.deviceService = deviceService;
+    }
+
+    @Autowired
+    public void setBrandService(BrandService brandService){
+        this.brandService = brandService;
     }
 
     @GetMapping("/")
@@ -47,7 +68,71 @@ public class DeviceController {
         return "devices";
     }
 
+    @GetMapping("/devices_page")
+    public ResponseEntity<Page<Device>> allDevices(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                   @RequestParam(value = "size", defaultValue = "5") int size) {
+        Page<Device> devicePage = deviceService.findAll(PageRequest.of(page, size));
 
+        // Kiểm tra xem trang có dữ liệu không
+        if (devicePage.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(devicePage, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/Admin/addDevices")
+    public ResponseEntity<Device> themDienThoai(@RequestParam Long brandId, @RequestParam String deviceName, @RequestParam(name = "releasedAt", required = false ) String releasedAt,
+                                                @RequestParam(name = "body", required = false ) String body,
+                                                @RequestPart(name = "file", required = false ) MultipartFile file,
+                                                @RequestParam(name = "os", required = false ) String os,
+                                                @RequestParam(name = "storage", required = false ) String storage,
+                                                @RequestParam(name = "displaySize", required = false ) String displaySize,
+                                                @RequestParam(name = "displayResolution", required = false ) String displayResolution,
+                                                @RequestParam(name = "cameraPixels", required = false ) String cameraPixels,
+                                                @RequestParam(name = "videoPixels", required = false ) String videoPixels,
+                                                @RequestParam(name = "ram", required = false ) String ram,
+                                                @RequestParam(name = "chipset", required = false ) String chipset,
+                                                @RequestParam(name = "batterySize", required = false ) String batterySize,
+                                                @RequestParam(name = "batteryType", required = false ) String batteryType,
+                                                @RequestParam(name = "specifications", required = false ) String specifications,
+                                                @RequestParam(name = "gia", required = false ) Double gia
+                                                ) throws IOException {
+        System.out.println("thành công1");
+        Device device = new Device();
+        device.setBrand(brandService.findById(brandId));
+        device.setDeviceName(deviceName);
+        device.setReleaseAt(releasedAt);
+        device.setBody(body);
+        System.out.println(resourceLoader);
+        Resource staticResource = resourceLoader.getResource("classpath:static/img");
+        System.out.println(file);
+        if (!file.isEmpty()) {
+            // Lưu ảnh vào máy chủ
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(staticResource.getURI()).resolve(file.getOriginalFilename());
+            Files.write(path, bytes);
+            device.setPicture(path.toString());
+            System.out.println("thành công2");
+        } else {
+            System.out.println("có lỗi");
+        }
+        device.setOs(os);
+        device.setStorage(storage);
+        device.setDisplaySize(displaySize);
+        device.setDisplayResolution(displayResolution);
+        device.setCameraPixels(cameraPixels);
+        device.setVideoPixels(videoPixels);
+        device.setRam(ram);
+        device.setChipset(chipset);
+        device.setBatterySize(batterySize);
+        device.setBatteryType(batteryType);
+        device.setSpecifications(specifications);
+        device.setGia(gia);
+        deviceService.save(device);
+        return ResponseEntity.ok(device);
+    }
 
 
 
--- SpringBoot-CuaHangDienThoai/src/main/resources/static/JavaScript/AdminPage.js
+++ SpringBoot-CuaHangDienThoai/src/main/resources/static/JavaScript/AdminPage.js
@@ -1,3 +1,106 @@
+function updateTable(data) {
+    var tbody = document.querySelector('#device-table tbody');
+    tbody.innerHTML = ''; // Xóa tất cả các hàng hiện có trong bảng
+    for (var i = 0; i < data.content.length; i++) {
+        let device = data.content[i];
+        var row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${device.id}</td>
+            <td>${device.deviceName}</td>
+            <td><img src="${device.picture}"></td>
+            <td>${device.gia}</td>
+            <td>${device.brand}</td>
+            <td>
+                <button class="btn btn-info">Infor</button>
+                <button class="btn btn-danger">Stop</button>
+            </td>
+        `;
+        tbody.appendChild(row);
+    }
+}
+function pageDevice(numberPage){
+    let xhr = new XMLHttpRequest();
+    let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+    let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+    url = '/devices_page?page=' + numberPage
+    console.log(url)
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader(csrfHeader,csrfToken);
+    xhr.send()
+    console.log("gửi yêu cầu")
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // Nếu dữ liệu được tải thành công, bạn có thể sử dụng JSON.parse() nếu dữ liệu trả về là JSON
+            let listDevice = JSON.parse(xhr.responseText);
+            console.log(listDevice)
+            updateTable(listDevice);
+            var divElement = document.createElement('div');
+            divElement.className = 'page';
+
+            var ulElement = document.createElement('ul');
+            ulElement.className = 'pagination';
+
+            // Thêm nút Previous (<<)
+            var liElement = document.createElement('li');
+            if (listDevice.number === 0) {
+                liElement.className = 'disabled';
+            }
+            var aElement = document.createElement('a');
+            aElement.href = '/admin?page=' + (listDevice.number - 1);
+            aElement.textContent = '<<';
+            liElement.appendChild(aElement);
+            ulElement.appendChild(liElement);
+
+            // Thêm các nút trang
+            for (var i = listDevice.number - 1; i <= listDevice.number + 2; i++) {
+                if (i >= 0 && i < listDevice.totalPages) {
+                    var liElement = document.createElement('li');
+                    var aElement = document.createElement('a');
+                    aElement.textContent = i + 1;
+                    aElement.addEventListener("click", ()=>{
+                        console.log("ádsa")
+                        console.log(i)
+                        pageDevice(i)
+                    })
+                    if((i) === listDevice.number){
+                        aElement.className = 'active-page'
+                    }
+                    liElement.appendChild(aElement);
+                    ulElement.appendChild(liElement);
+                }
+            }
+
+            // Thêm dấu '...'
+            if (listDevice.number < listDevice.totalPages - 3) {
+                var liElement = document.createElement('li');
+                var spanElement = document.createElement('span');
+                spanElement.textContent = '...';
+                liElement.appendChild(spanElement);
+                ulElement.appendChild(liElement);
+            }
+            var liElement = document.createElement('li');
+            var aElement = document.createElement('a');
+            aElement.href = '/admin?page=' + (listDevice.totalPages - 1);
+            aElement.textContent = listDevice.totalPages - 1;
+            liElement.appendChild(aElement);
+            ulElement.appendChild(liElement);
+            // Thêm nút Last (>>)
+            var liElement = document.createElement('li');
+            if (listDevice.number === listDevice.totalPages - 1) {
+                liElement.className = 'disabled';
+            }
+            var aElement = document.createElement('a');
+            aElement.href = '/admin?page=' + (listDevice.totalPages - 1);
+            aElement.textContent = ">>";
+            liElement.appendChild(aElement);
+            ulElement.appendChild(liElement);
+            let page = document.querySelector("#device")
+            page.appendChild(ulElement)
+        } else {
+            console.error('Lỗi khi tải dữ liệu từ "Admin/devices":', xhr.status, xhr.statusText);
+        }
+    };
+}
 
 const TITLE = document.querySelector("header").innerText;
 if(TITLE == "QUẢN LÝ ĐƠN HÀNG"){
@@ -7,6 +110,7 @@ if(TITLE == "QUẢN LÝ ĐƠN HÀNG"){
 else if(TITLE == "QUẢN LÝ SẢN PHẨM"){
     let page = document.querySelector("#device")
     page.style.display = "block"
+    pageDevice(0)
 }
 else if(TITLE == "ADMIN"){
     let page = document.querySelector("#admin-acc")
@@ -112,6 +216,10 @@ modalInforBtns.forEach((btn) => {
             btnYes.setAttribute("data-id",btn.getAttribute("data-id"))
             btnYes.setAttribute("data-func","orderCancel")
         }
+        else if(btn.getAttribute("data-target").toString() == "#ModalCreateAdmin" && (TITLE == "ADMIN")){
+            titleModal = modal.querySelector("h2")
+            titleModal.innerText = "Tạo tài khoản admin"
+        }
         else if(btn.getAttribute("data-target").toString() == "#ModalYesNo" && (TITLE == "ADMIN") && btn.getAttribute("btn-use") =="lock"){
             titleModal = modal.querySelector("h2")
             titleModal.innerText = "Khóa tài khoản có username : "+ btn.getAttribute("data-id").toString() + "?";
@@ -132,6 +240,29 @@ modalInforBtns.forEach((btn) => {
             btnChangPass = modal.querySelector("button")
             btnChangPass.setAttribute("username", btn.getAttribute("data-id"))
         }
+        else if(btn.getAttribute("data-target").toString() == "#ModalAddDT" && (TITLE == "QUẢN LÝ SẢN PHẨM")){
+            titleModal = modal.querySelector("h2")
+            titleModal.innerText = "Thêm sản phẩm"
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "/brand", true)
+            xhr.setRequestHeader("content-Type", "application/json")
+            let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+            let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+            xhr.setRequestHeader(csrfHeader,csrfToken);
+            xhr.send();
+            xhr.onreadystatechange = function (){
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    const responseEntity = JSON.parse(xhr.responseText)
+                    const brandId = document.querySelector("#brand_id")
+                    for (let brand of responseEntity){
+                        option = document.createElement("option")
+                        option.value = brand.id;
+                        option.innerText = brand.brandName;
+                        brandId.appendChild(option)
+                    }
+                }
+            }
+        }
     })
 })
 /* xư lý riêng phần đổi mật khẩu  */
@@ -147,17 +278,6 @@ btnChangPass.addEventListener("click", () =>{
     let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
     let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
     xhr.setRequestHeader(csrfHeader,csrfToken);
-    const requestParams = [
-        {
-            key: "userName",
-            value: userName
-        },
-        {
-            key: "pass",
-            value: pass
-        }
-    ];
-    console.log(requestParams)
     xhr.send("userName=" + encodeURIComponent(userName)+"&pass=" + encodeURIComponent(pass));
     xhr.onreadystatechange = function (){
         if(xhr.readyState === 4 && xhr.status === 200){
@@ -309,4 +429,85 @@ async function creatrAdmin(){
 
 }
 
-document.querySelector("button[type='submit']").addEventListener("click",creatrAdmin)
+function addDevices(){
+    let brand_id = document.querySelector("#brand_id");
+    let selectedOption = brand_id.options[brand_id.selectedIndex];
+    let selectedValue = selectedOption.value;
+    let device_name = document.querySelector("input[name = 'device_name']").value;
+    let released_at = document.querySelector("input[name = 'released_at']").value;
+    let body = document.querySelector("input[name = 'body']").value;
+    let file = document.querySelector("input[name = 'picture']").files[0];
+    let os = document.querySelector("input[name = 'os']").value;
+    let storage = document.querySelector("input[name = 'storage']").value;
+    let display_size = document.querySelector("input[name = 'display_size']").value;
+    let display_resolution = document.querySelector("input[name = 'display_resolution']").value;
+    let camera_pixels = document.querySelector("input[name = 'camera_pixels']").value;
+    let video_pixels = document.querySelector("input[name = 'video_pixels']").value;
+    let ram = document.querySelector("input[name = 'ram']").value;
+    let chipset = document.querySelector("input[name = 'chipset']").value;
+    let battery_size = document.querySelector("input[name = 'battery_size']").value;
+    let battery_type = document.querySelector("input[name = 'battery_type']").value;
+    let specifications = document.querySelector("input[name = 'specifications']").value;
+    let gia = document.querySelector("input[name = 'gia']").value;
+    const xhr = new XMLHttpRequest();
+    let formData =   new FormData();
+    xhr.open("POST", "/Admin/addDevices", true)
+    let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+    let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+    formData.append("brandId", parseInt(selectedValue, 10));
+    formData.append("deviceName", device_name);
+    formData.append("releasedAt", released_at);
+    formData.append("body", body);
+    formData.append("file", file);
+    formData.append("os", os);
+    formData.append("storage", storage);
+    formData.append("displaySize", display_size);
+    formData.append("displayResolution", display_resolution);
+    formData.append("cameraPixels", camera_pixels);
+    formData.append("videoPixels", video_pixels);
+    formData.append("ram", ram);
+    formData.append("chipset", chipset);
+    formData.append("batterySize", battery_size);
+    formData.append("batteryType", battery_type);
+    formData.append("specifications", specifications);
+    formData.append("gia", gia);
+    xhr.setRequestHeader(csrfHeader,csrfToken);
+    console.log(device_name)
+    console.log(file)
+    console.log(specifications)
+    var formDataEntries = formData.entries();
+    for (var pair of formDataEntries) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+    xhr.send(formData);
+    xhr.onreadystatechange = function (){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            console.log('tạo sản phẩm thành công')
+            location.reload()
+        }
+    }
+}
+
+
+btnSubmit = document.querySelectorAll("button[type='submit']")
+btnSubmit.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        name = btn.getAttribute("name")
+        if(name == "createAdmin"){
+            creatrAdmin();
+        }
+        else if (name == "addDevices"){
+            addDevices()
+        }
+    })
+})
+
+
+/* Chọn ảnh trong thêm điện thoại*/
+
+document.querySelector("input[name='picture']").addEventListener("change", function(){
+    let file = this.files[0];
+    let url = URL.createObjectURL(file)
+    img = document.querySelector("#img-modal-add img")
+    img.src = url
+})
