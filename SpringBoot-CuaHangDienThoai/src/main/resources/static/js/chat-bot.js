e7d54acd575bfca506edbfca192e9425f23500bb
spidicey <82193435+spidicey@users.noreply.github.com>
2023-10-31 14:23:54 +0700

    add chat-bot

--- SpringBoot-CuaHangDienThoai/src/main/resources/static/js/chat-bot.js
+++ SpringBoot-CuaHangDienThoai/src/main/resources/static/js/chat-bot.js
@@ -6,9 +6,25 @@ $(document).ready(() => {
 
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
-console.log(chatInput, sendChatBtn);
+const chatBox = document.querySelector(".chat-box");
+
+const createLi = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outcoming"
+      ? `<p>${message}</p>`
+      : `<span class = "material-symbols-outlined"> smart_toy </span>
+    <p>${message}</p>`;
+  chatLi.innerHTML = chatContent;
+  return chatLi;
+};
 const handleChat = () => {
   let userMessage = chatInput.value.trim();
+  if (!userMessage) return;
+  chatBox.appendChild(createLi(userMessage, "outcoming"));
+  chatBox.appendChild(createLi(userMessage, "incoming"));
+  chatInput.value = "";
   console.log(userMessage);
 };
 sendChatBtn.addEventListener("click", handleChat);
